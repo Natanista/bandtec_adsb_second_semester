c161d307007f2705033831ebcaaa6b1ae04bd89f
natanista <gdaynatan@gmail.com>
2021-03-01 14:33:45 -0300

    aula de metodos

--- Linguagem_Programacao/nivelamento-java2/src/main/java/br/com/bandtec/nivelamento/java2/ContadorVariado.java
+++ Linguagem_Programacao/nivelamento-java2/src/main/java/br/com/bandtec/nivelamento/java2/ContadorVariado.java
@@ -3,7 +3,7 @@ package br.com.bandtec.nivelamento.java2;
 public class ContadorVariado {
     public static void main(String[] args) {
 
-        for(double i = 0.15; i < 5; i = i + 0.15){
+        for(double i = 0.15; i < 5; i += 0.15){
             System.out.println(String.format("%.2f",i));
         }
     }
--- README.md
+++
@@ -1,2 +0,0 @@
-# bandtec_adsb_second_semester
-exercícios referentes ao segundo semestre do curso de analise e desenvolvimento de sistemas
